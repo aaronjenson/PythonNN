d4952deb66609193913fe53ffa9d812346c57f0c
Aaron Jenson <aaronajenson@gmail.com>
2018-06-20 17:25:56 -0700

    Initial commit, working neural network w/ xor data used for testing.

---
+++ test.py
@@ -0,0 +1,20 @@
+import nn
+
+data = [[[0, 0], [0]],
+        [[0, 1], [1]],
+        [[1, 1], [0]],
+        [[1, 0], [1]]]
+
+
+def main():
+    neural_network = nn.NeuralNetwork(2, 4, 1)
+    neural_network.train(data, 5000, 0.0001)
+
+    print('[0, 0] => %-.5f' % neural_network.feed_forward([0, 0])[0])
+    print('[0, 1] => %-.5f' % neural_network.feed_forward([0, 1])[0])
+    print('[1, 1] => %-.5f' % neural_network.feed_forward([1, 1])[0])
+    print('[1, 0] => %-.5f' % neural_network.feed_forward([1, 0])[0])
+
+
+if __name__ == '__main__':
+    main()
